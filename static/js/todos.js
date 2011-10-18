f8a965bc157cdcf1b36f56e33763182e4b050c5b
Larry Myers <larry@larrymyers.com>
2011-10-17 22:08:03 -0400

    move tests into test folder

--- static/js/todos.js
+++ static/js/todos.js
@@ -165,10 +165,14 @@
         showTooltip: function(e) {
             var tooltip = this.$(".ui-tooltip-top");
             var val = this.input.val();
+            
             tooltip.fadeOut();
-            if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
-            if (val == '' || val == this.input.attr('placeholder')) return;
+            
+            if (this.tooltipTimeout) { clearTimeout(this.tooltipTimeout); }
+            if (val == '' || val == this.input.attr('placeholder')) { return; }
+            
             var show = function(){ tooltip.show().fadeIn(); };
+            
             this.tooltipTimeout = _.delay(show, 1000);
         }
         
